e950d14c613b06c6da9fa81d272755d50172f836
Trần Thành Phương Đăng <phuongdang012@gmail.com>
2021-05-13 22:39:11 +0700

    Log stringify request body for debugging

--- auto-deploy.js
+++ auto-deploy.js
@@ -33,7 +33,9 @@ const isCorrectGitServer = (request, response) => {
   switch (config.gitServerType) {
     case "GitHub":
       var signer = crypto.createHmac("sha1", config.webhookSecret);
-      var result = signer.update(JSON.stringify(request.data)).digest("hex");
+      var stringifyBody = JSON.stringify(request.body);
+      console.log(stringifyBody);
+      var result = signer.update(stringifyBody).digest("hex");
       const signature = request.headers["X-Hub-Signature"];
 
       if (signature !== expectedSignature) {
